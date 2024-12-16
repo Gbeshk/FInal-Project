1e9d707c1fa1344b555f3cc46806c436522c2a14
Lewyyshub <salamashvili21@gmail.com>
2024-12-16 19:40:35 +0400

    feat(CREATED:STRUCTURE) added responsive on second page

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let menuButton = document.getElementById("menu_button");
-menuButton.addEventListener("click" => {
-    
-})
+// var Header = document.getElementById("head");
+// var secondPage = document.querySelector(".second_page");
+
+// document.getElementById("menu_button").addEventListener("click", function () {
+//   if (Header.style.display === "flex") {
+//     Header.style.display = "flex";
+//     secondPage.style.display = "none";
+//   } else {
+//     Header.style.display === "none";
+//     secondPage.style.display = "flex";
+//   }
+// });
