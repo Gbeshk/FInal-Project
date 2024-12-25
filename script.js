6510f4b620d72d69cfc35ea70a93d72e63b0af97
Gbeshk <giorgibeshkenadze4@gmail.com>
2024-12-25 19:52:41 +0400

    feat:(JS, CSS, HTML):all done

--- script.js
+++ script.js
@@ -97,13 +97,33 @@ section3div5.addEventListener("mouseleave", () => {
   let divlinesection3 = document.getElementById("divlinesection3_5");
   divlinesection3.style.backgroundColor = " rgba(255, 255, 255, 0.2)";
 });
-let foto;
 async function getAllProducts() {
   try {
     let data = await fetch("https://fakestoreapi.com/products");
     let products = await data.json();
-    foto = products[0].image;
     for (let i = 1; i <= 12; i++) {
+      let justimage = document.getElementById("justimage");
+      justimage.src = products[0].image;
+      let pdiv = document.getElementById("section3pdiv1");
+      pdiv.addEventListener("mouseenter", () => {
+        justimage.src = products[1].image;
+      });
+      let pdiv2 = document.getElementById("section3pdiv2");
+      pdiv2.addEventListener("mouseenter", () => {
+        justimage.src = products[2].image;
+      });
+      let pdiv3 = document.getElementById("section3pdiv3");
+      pdiv3.addEventListener("mouseenter", () => {
+        justimage.src = products[3].image;
+      });
+      let pdiv4 = document.getElementById("section3pdiv4");
+      pdiv4.addEventListener("mouseenter", () => {
+        justimage.src = products[4].image;
+      });
+      let pdiv5 = document.getElementById("section3pdiv5");
+      pdiv5.addEventListener("mouseenter", () => {
+        justimage.src = products[5].image;
+      });
       let mobilesliderp = document.getElementById(`mobilesliperp${i}`);
       let mobilesliderpp = document.getElementById(`mobilesliperpp${i}`);
       let mobilesliderppp = document.getElementById(`mobilesliperppp${i}`);
@@ -199,44 +219,61 @@ async function getAllProducts() {
       secondswiperppp.textContent = products[i].price;
       secondswiperppp.textContent = secondswiperppp.textContent.slice(0, 50);
     }
-    let justimage = document.getElementById("justimage");
-    justimage.src = "./images/image.jpg";
-
-    for (let i = 1; i <= 5; i++) {
-      let pdiv = document.getElementById(`section3pdiv${i}`);
-      pdiv.addEventListener("mouseover", () => {
-        justimage.src = products[i].image;
-      });
-      pdiv.addEventListener("mouseleave", () => {
-        justimage.src = "./images/image.jpg";
-      });
-    }
   } catch (error) {
     console.log(error);
   }
 }
 getAllProducts();
+let dragslider = document.getElementById("mobileslider");
+let isDragging = false;
+let startX;
+let scrollLeft;
+
+dragslider.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  dragslider.style.cursor = "pointer";
+  startX = e.pageX - dragslider.offsetLeft;
+  scrollLeft = dragslider.scrollLeft;
+});
+
+dragslider.addEventListener("mouseleave", () => {
+  isDragging = false;
+  dragslider.style.cursor = "pointer";
+});
+
+dragslider.addEventListener("mouseup", () => {
+  isDragging = false;
+  dragslider.style.cursor = "pointer";
+});
+
+dragslider.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  const x = e.pageX - dragslider.offsetLeft;
+  const walk = (x - startX) * 2;
+  dragslider.scrollLeft = scrollLeft - walk;
+});
 let swiperpaginationactive = document.getElementsByClassName(
   "swiper-pagination-bullet swiper-pagination-bullet-active"
 );
 let moreworks = document.getElementById("moreworks");
 moreworks.addEventListener("click", () => {
   document.documentElement.scrollTo({
-    top: 884,
+    top: 1750,
     behavior: "smooth",
   });
 });
 let viewworks = document.getElementById("viewworks");
 viewworks.addEventListener("click", () => {
   document.documentElement.scrollTo({
-    top: 884,
+    top: 1750,
     behavior: "smooth",
   });
 });
 let buttonmobile = document.getElementById("buttonmobile");
 buttonmobile.addEventListener("click", () => {
   document.documentElement.scrollTo({
-    top: 700,
+    top: 1750,
     behavior: "smooth",
   });
 });
@@ -252,12 +289,142 @@ buttonmobile.addEventListener("mouseleave", () => {
 let morestories = document.getElementById("morestories");
 morestories.addEventListener("click", () => {
   document.documentElement.scrollTo({
-    top: 884,
+    top: 1750,
     behavior: "smooth",
   });
 });
-
+let letswork = document.getElementById("letswork");
+letswork.addEventListener("click", () => {
+  document.documentElement.scrollTo({
+    top: 1750,
+    behavior: "smooth",
+  });
+});
 let call = document.getElementById("call");
 call.addEventListener("click", () => {
   window.location.href = "tel:+995551537703";
+});
+let letswork2 = document.getElementById("letswork2");
+letswork2.addEventListener("click", () => {
+  document.documentElement.scrollTo({
+    top: 2000,
+    behavior: "smooth",
+  });
+});
+let arrowdown2 = document.getElementById("arrowdown2");
+arrowdown2.addEventListener("click", () => {
+  document.documentElement.scrollTo({
+    top: 2000,
+    behavior: "smooth",
+  });
+});
+let menubutton = document.getElementById("menubutton");
+menubutton.addEventListener("click", () => {
+  let line = document.getElementById("menubuttonline1");
+  line.classList.toggle("menubuttonlinee");
+  let line2 = document.getElementById("menubuttonline2");
+  line2.classList.toggle("menubuttonlinee2");
+  let headercontainer = document.getElementById("headercontainer");
+  headercontainer.classList.toggle("headercontainerr");
+  let hiddendiv = document.getElementById("hiddendiv");
+  hiddendiv.classList.toggle("hiddendivv");
+  let headerlist = document.getElementById("headerlist");
+  headerlist.classList.toggle("headerlistt");
+  let headerrightlist = document.getElementById("headerrightlist");
+  headerrightlist.classList.toggle("headerrightlistt");
+  let positionneuro = document.getElementById("positionneuro");
+  positionneuro.classList.toggle("positionneuroo");
+  let headermaindiv = document.getElementById("headermaindiv");
+  headermaindiv.classList.toggle("headermaindivv");
+  let inc = document.getElementById("inc");
+  inc.classList.toggle("incc");
+  let wor = document.getElementById("wor");
+  wor.classList.toggle("worr");
+  let menu = document.getElementById("menu");
+  menu.classList.toggle("menuu");
+  let menubutton = document.getElementById("menubutton");
+  menubutton.classList.toggle("menubuttonn");
+});
+let arrowdown = document.getElementById("arrowdown");
+arrowdown.addEventListener("click", () => {
+  let bigcontainer = document.getElementById("bigcontainer");
+  bigcontainer.style.display = "inline";
+  let footer = document.getElementById("footer");
+  footer.style.display = "inline";
+  document.documentElement.scrollTo({
+    top: 1000,
+    behavior: "smooth",
+  });
+});
+function Today() {
+  let today = new Date();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let dayOfWeek = days[today.getDay()];
+  let dayOfMonth = today.getDate().toString().padStart(2, "0");
+  let month = months[today.getMonth()];
+  return `${dayOfWeek} ${dayOfMonth}.${month.slice(0, 3)}`;
+}
+let dge = Today();
+let dgee = document.getElementById("dge");
+dgee.textContent = dge;
+let mobilemenu = document.getElementById("mobilemenu");
+mobilemenu.addEventListener("click", () => {
+  let mobilehiddendiv = document.getElementById("mobilehiddendiv");
+  mobilehiddendiv.classList.toggle("mobilehiddendivv");
+  let mobilelists = document.getElementById("mobilelists");
+  mobilelists.style.transition = "1s";
+  mobilelists.classList.toggle("mobilelistss");
+  let hiddenmobilelastdiv = document.getElementById("hiddenmobilelastdiv");
+  hiddenmobilelastdiv.classList.toggle("hiddenmobilelastdivv");
+  let mobileheadergraydiv = document.getElementById("mobileheadergraydiv");
+  mobileheadergraydiv.classList.toggle("mobileheadergraydivv");
+  let mobilemenuline = document.getElementById("mobilemenuline1");
+  mobilemenuline.classList.toggle("mobilemenulinee");
+  let mobilemenuline2 = document.getElementById("mobilemenuline2");
+  mobilemenuline2.classList.toggle("mobilemenulinee2");
+  let mobilemenu = document.getElementById("mobilemenu");
+  mobilemenu.classList.toggle("mobilemenuu");
+});
+let xx = localStorage.getItem("mode");
+if (xx % 2 == 1) {
+  document.body.classList.toggle("dark-theme");
+  let werto = document.getElementById("werto");
+  werto.classList.toggle("wertilaa");
+  let wertomobile = document.getElementById("wertomobile");
+  wertomobile.classList.toggle("wertilaa");
+}
+let buttondiv = document.getElementById("buttondiv");
+buttondiv.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  let werto = document.getElementById("werto");
+  werto.classList.toggle("wertilaa");
+  xx++;
+  localStorage.setItem("mode", xx);
+  let wertomobile = document.getElementById("wertomobile");
+  wertomobile.classList.toggle("wertilaa");
+});
+let mobilebuttondiv = document.getElementById("mobilebuttondiv");
+mobilebuttondiv.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  let werto = document.getElementById("wertomobile");
+  werto.classList.toggle("wertilaa");
+  xx++;
+  localStorage.setItem("mode", xx);
+  let mobilewerto = document.getElementById("werto");
+  mobilewerto.classList.toggle("wertilaa");
 });
